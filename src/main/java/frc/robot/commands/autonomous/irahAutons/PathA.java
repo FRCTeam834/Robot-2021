e39d2c2daa2bf317e4b2531f32073eb423a059c0
JadonTrackim <43653946+JadonTrackim@users.noreply.github.com>
2021-03-01 20:14:19 -0500

    finished Path A

--- src/main/java/frc/robot/commands/autonomous/irahAutons/PathA.java
+++ src/main/java/frc/robot/commands/autonomous/irahAutons/PathA.java
@@ -22,9 +22,9 @@ import frc.robot.commands.autonomous.*;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class PathB extends SequentialCommandGroup {
+public class PathA extends SequentialCommandGroup {
   /** Creates a new PathB. */
-  public PathB() {
+  public PathA() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
 
@@ -32,10 +32,10 @@ public class PathB extends SequentialCommandGroup {
     Starting position: D1
     *detect distance to nearest ball(either d5 or d6) if distance = 1.2m its red path if = 1.5m its blue path*
     */
-    int angle1 = 0;
-    int angle2 = 0;
-    int angle3 = 0;
-    int angle4 = 0;
+    double angle1 = 0;
+    double angle2 = 0;
+    double angle3 = 0;
+    double angle4 = 0;
     // tbh we don't know what the distances are in
     double distance1 = 0;
     double distance2 = 0;
@@ -43,35 +43,35 @@ public class PathB extends SequentialCommandGroup {
     double distance4 = 0;
     
     // Red Path
-    if(RobotContainer.ultrasonicSensor.withinRange(0.9, 0.1)) {
-      // Turn 45 degrees to the left; Move 84.85 cm Forwards
-        angle1 = 45; distance1 = .8485;
+    if(RobotContainer.ultrasonicSensor.withinRange(0.6, 0.1)) {
+      // Move 60 cm Forwards
+        angle1 = 0; distance1 = .6;
 
-      // Turn 90 degrees to the right; Move 84.85 cm Forwards
-        angle2 = 90; distance2 = .8485;
+      // Turn 26.565 degrees to the right; Move 67.08 cm Forwards
+        angle2 = -26.565; distance2 = .6708;
         
-      // Turn 90 degrees to the left; Move 84.85 cm Forwards
-        angle3 = 90; distance3 = .8485;
+      // Turn 98.13 degrees to the left; Move 94.868 cm Forwards
+        angle3 = 71.565; distance3 = .94868;
 
-      // Turn 45 degrees to the right; Move 130 cm Forwards
-        angle4 = 45; distance4 = .8485;
+      // Turn 71.565 degrees to the right; Move 130 cm Forwards
+        angle4 = 0; distance4 = 160;
       
       // Makes lights red because red path
         Robot.lights = Constants.LAVA_RAINBOW;
     }
     // Blue Path
-    else if(RobotContainer.ultrasonicSensor.withinRange(1.2, 0.1)) {
-      // Move Forwards 150 cm
-        angle1 = 0; distance1 = 1.20;
+    else if(RobotContainer.ultrasonicSensor.withinRange(2.4, 0.1)) {
+      // Turn  degrees to the right; Move Forwards 161.555 cm
+        angle1 = -21.801; distance1 = 1.61555;
       
-      // Turn 45 degrees to the left; Move 84.85 cm Forwards
-        angle2 = 45; distance2 = .8485;
+      // Turn 93.365 degrees to the left; Move 94.868 cm Forwards
+        angle2 = 71.565; distance2 = .94868;
       
-      // Turn 90 degrees to the right; Move 84.85 cm Forwards
-        angle3 = 90; distance3 = .8485;
+      // Turn 98.13 degrees to the right; Move 67.08 cm Forwards
+        angle3 = -26.565; distance3 = .6708;
       
-      // Turn 45 degrees to the left; Move 40 cm Forwards
-        angle4 = 45; distance4 = .40;
+      // Turn 26.565 degrees to the left
+        angle4 = 0; distance4 = .7;
 
       // LED
         Robot.lights = Constants.SKY_BLUE;
